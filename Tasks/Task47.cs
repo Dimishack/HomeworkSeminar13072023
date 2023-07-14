43feb667c2e7960e9b38b48f3bff633102b43818
Dimishack <dimishack.git@mail.ru>
2023-07-14 15:24:50 +0300

    Выполнение задачи №47

--- Tasks/Task47.cs
+++ Tasks/Task47.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeworkSeminar13072023.Tasks
 {
@@ -18,7 +13,39 @@ namespace HomeworkSeminar13072023.Tasks
     {
         internal static void TwoDimensionalArray()
         {
+            uint GetQuantity(string message)
+            {
+                uint quantity;
 
+                Console.WriteLine("(Примечание: число должно быть положительным)");
+                while (true)
+                {
+                    Console.Write("Введите {0}: ", message);
+                    if (uint.TryParse(Console.ReadLine(), out quantity))
+                        break;
+                    Console.WriteLine("Данное значение не является положительным числом!");
+                }
+                return quantity;
+            }
+
+            Random rnd = new();
+            uint row, column;
+
+            Console.WriteLine("Задача 47. Задайте двумерный массив размером mxn, заполненный случайными вещественными числами." + 
+                                "\n" + new string('-', 50));
+            row = GetQuantity("m (количество строк)");
+            column = GetQuantity("n (количество столбцов)");
+            double[,] array = new double[row, column];
+            Console.WriteLine("Двумерный массив вещественных чисел:");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    array[i, j] = Math.Round(rnd.NextDouble() * Math.Exp(rnd.Next(1, 4)), 2);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
